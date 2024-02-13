ff7599fafe5bba82b05d256ce598b73d72e6e83a
tupudipi <tupudipi@gmail.com>
2024-02-13 21:53:58 +0200

    Update authentication links and responsive menu

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+"use client"
+
+import { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from 'next/link';
 
 function AuthLinks() {
 
+  const [open, setOpen] = useState(false)
+
   //temporary status
-  const status = 'authenticated'
+  const status = 'not authenticated'
 
   return <>
     {status === 'not authenticated' ? (
-      <Link href='/login'>Login</Link>
+      <Link href='/login' className={styles.link}>Login</Link>
     ) : (
       <>
-        <Link href='/write'>Write</Link>
+        <Link href='/write' className={styles.link}>Write</Link>
         <span className={styles.link}>Logout</span>
       </>
+    )}
+    <div className={styles.burger} onClick={() => setOpen(!open)}>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+    </div>
+    {open && (
+      <div className={styles.responsiveMenu}>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/contact'>Contact</Link>
+        {status === 'not authenticated' ? (
+          <Link href='/login'>Login</Link>
+        ) : (
+          <>
+            <Link href='/write'>Write</Link>
+            <span className={styles.link}>Logout</span>
+          </>
+        )}
+      </div>
     )}
   </>
 }
--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -22,9 +22,9 @@ const Navbar = () => {
 
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Home</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link href="/" className={styles.link}>Home</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
         <AuthLinks />
       </div>
 
