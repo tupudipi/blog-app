de7a623e1be9e0c760d4585867275cacf9a1e22e
tupudipi <tupudipi@gmail.com>
2024-02-13 21:24:40 +0200

    added theme toggle, theme context and provider

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client"
 import styles from "./themeToggle.module.css";
+import Image from 'next/image';
+import { useContext } from 'react';
+import { ThemeContext } from '../../../context/ThemeContext';
 
-function ThemeToggle() {
+const ThemeToggle = () => {
+
+  const { toggleTheme, theme } = useContext(ThemeContext);
+  console.log(theme)
+
+
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container} onClick={toggleTheme}>
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === 'dark'
+            ? { left: 0, background: "#0f172a" } :
+            { right: 0, background: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
   )
 }
 
---
+++ src/providers/ThemeProvider.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useContext, useState, useEffect } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+
+const ThemeProvider = ({ children }) => {
+
+  const { theme } = useContext(ThemeContext);
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) {
+    return <div className={theme}>{children}</div>
+  }
+};
+
+export default ThemeProvider
