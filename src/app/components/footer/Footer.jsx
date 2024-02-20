7913195bce0af3d960b8ad39c39cc7ae98f10fb3
tupudipi <tupudipi@gmail.com>
2024-02-20 11:42:51 +0200

    added footer structure and responsive design

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image'
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className={styles.container}>footer</div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="Cico Blog Logo" width={50} height={50} />
+          <h2 className={styles.logoText}>Cico Blog</h2>
+        </div>
+        <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        <div className={styles.icons}>
+        <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+        <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+        <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+        <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </div>
+      </div>
+
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Culture</Link>
+          <Link href="/">Leisure</Link>
+          <Link href="/">Food</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
