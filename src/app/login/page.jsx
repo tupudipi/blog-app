4a847fb01961203f4e8b3aaad12e4a58e6e0612d
tupudipi <tupudipi@gmail.com>
2024-02-24 19:39:27 +0200

    Remove unused social login buttons

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -21,12 +21,6 @@ const LoginPage = () => {
                 <div className={styles.socialButton} onClick={()=>signIn("google")}>
                     Sign in with Google
                 </div>
-                <div className={styles.socialButton}>
-                    Sign in with Github
-                </div>
-                <div className={styles.socialButton}>
-                    Sign in with Facebook
-                </div>
             </div>
         </div>
     )
